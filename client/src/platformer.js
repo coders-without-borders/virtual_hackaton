9fe5d04d381e8eb2ad4c6ad78dc3c00f3c157987
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2016-02-20 15:31:39 -0200

    add world class

--- client/src/platformer.js
+++ client/src/platformer.js
@@ -5,13 +5,29 @@
  * prototypes, global values and helper functions
  */
 var Platformer = Platformer || {
-    unit: 50.0,
-    startPosition: {
-        x: 100,
-        y: 100,
-    },
+    unit: 35.0,
     game: null,
     gravity: 300,
+    padding: 100,
+    speed: {
+        walk: 185,
+        jump: 200,
+    },
+};
+
+Platformer.Color = function(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = 255;
+};
+
+Platformer.Color.fromHex = function(hex) {
+    var bigint = parseInt(hex.substr(1), 16);
+    return new Platformer.Color(
+        (bigint >> 16) & 255,
+        (bigint >> 8) & 255,
+        bigint & 255);
 };
 
 /**
@@ -20,51 +36,119 @@ var Platformer = Platformer || {
 Platformer.init = function() {
     window.Platformer = Platformer;
     Platformer.game.physics.startSystem(Phaser.Physics.ARCADE);
+    Platformer.game.physics.arcade.gravity.y = Platformer.gravity;
 }
 
 /**
  * A helper function to create a square with physics.
  */
-Platformer.createSquare = function(x, y, width, height, r, g, b) {
-    var graphics = Platformer.game.add.bitmapData(width, height);
-    graphics.fill(r, g, b,255);
-
-    var square = Platformer.game.add.sprite(0, 0, graphics);
+Platformer.createSquare = function(pos, color) {
+    var graphics = Platformer.game.add.bitmapData(Platformer.unit, Platformer.unit);
+    graphics.fill(color.r, color.g, color.b, color.a);
+    var square = Platformer.game.add.sprite(pos.x, pos.y, graphics);
     Platformer.game.physics.arcade.enable(square);
 
     return square;
 }
 
 /**
+ * The World object
+ */
+Platformer.World = function() {
+    this.player = new Platformer.Player({x: 400, y: 800});
+    this.tiles = [];
+    this.bounds = {
+        min: {x: 0, y: 0},
+        max: {x: 0, y: 0},
+    };
+};
+
+Platformer.World.getPos = function(x, y) {
+    return {
+        x: x * Platformer.unit + Platformer.padding,
+        y: y * Platformer.unit + Platformer.padding,
+    };
+};
+
+Platformer.World.prototype = {
+    start: function(level) {
+        var that = this;
+        level.tiles.forEach(function(tile) {
+            var position = Platformer.World.getPos(tile.position[0], tile.position[1]);
+
+            var square = Platformer.createSquare(
+                position, Platformer.Color.fromHex(tile.color));
+
+            var max = {
+                x: position.x + Platformer.unit,
+                y: position.y + Platformer.unit,
+            };
+            if(max.x > that.bounds.max.x) {
+                that.bounds.max.x = max.x;
+            }
+            if(max.y > that.bounds.max.y) {
+                that.bounds.max.y = max.y;
+            }
+
+            square.body.allowGravity = false;
+            square.body.immovable = true;
+
+            that.tiles.push(square);
+        });
+
+        Platformer.game.world.setBounds(
+            this.bounds.min.x, this.bounds.min.y,
+            this.bounds.max.x + Platformer.padding * 2,
+            this.bounds.max.y + Platformer.padding * 2);
+    },
+
+    update: function() {
+        var that = this;
+        this.tiles.forEach(function(tile) {
+            that.player.collide(tile);
+        });
+
+        this.player.update();
+    },
+};
+
+/**
  * The player object.
  */
-Platformer.Player = function () {
+Platformer.Player = function(pos) {
     this.square = Platformer.createSquare(
-        Platformer.startPosition.x, Platformer.startPosition.y,
-        Platformer.unit, Platformer.unit, 255, 0, 0);
+        pos, Platformer.Color.fromHex("#FFFF00"));
 
-    this.square.body.bounce.y = 0.1;
-    this.square.body.gravity.y = Platformer.gravity;
+    Platformer.game.camera.follow(this.square);
+
+    this.square.body.bounce.y = 0;
     this.square.body.collideWorldBounds = true;
+
+    this.cursors = Platformer.game.input.keyboard.createCursorKeys();
+    this.jumpButton = Platformer.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 };
 
 Platformer.Player.prototype = {
     update: function() {
-        var cursors = Platformer.game.input.keyboard.createCursorKeys();
 
         this.square.body.velocity.x = 0;
-        if (cursors.left.isDown)
+        if (this.cursors.left.isDown)
         {
-            this.square.body.velocity.x = -150;
+            this.square.body.velocity.x = -Platformer.speed.walk;
         }
-        else if (cursors.right.isDown)
+        else if (this.cursors.right.isDown)
         {
-            this.square.body.velocity.x = 150;
+            this.square.body.velocity.x = Platformer.speed.walk;
         }
 
-        if (cursors.up.isDown && this.square.body.touching.down)
+        var allowToJump = true;
+        if (this.jumpButton.isDown && allowToJump)
         {
-            this.square.body.velocity.y = -350;
+            this.square.body.velocity.y = -Platformer.speed.jump;
         }
     },
+
+    collide: function(other) {
+        Platformer.game.physics.arcade.collide(this.square, other);
+    }
 };
