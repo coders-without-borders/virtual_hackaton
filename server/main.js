1861888f41e7d0d6c13cc33ed82455ef665160a6
PieterVantorre <pietervantorre@gmail.com>
2016-02-20 16:39:03 +0100

    add new votes to the database

--- server/main.js
+++ server/main.js
@@ -7,16 +7,23 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('The backend server');
 });
 
 app.post('/votes/vote_for_repo', votes.vote_for_repo );
+app.get('/votes/get_top_repos', votes.get_top_repos);
 
 database.initialize();
 
 var server = app.listen(3000, function() {
     console.log("server listening on port 3000");
-})
+});
 
 console.log("NodeJS web server running on 0.0.0.0:3000");
--- server/votes.js
+++ server/votes.js
@@ -10,15 +10,37 @@
             'INSERT OR REPLACE INTO votes(idRepo, votes) VALUES( ?, COALESCE((SELECT votes FROM votes WHERE idRepo = ?), 0) + 1);',
             data.repo_url,
             data.repo_url,
-            function( err, row ) {
+            function( err ) {
                 if ( err ) {
                     console.log(err);
                 } else {
                     console.log("Voted for repository" );
+                }
+                res.send("");
+            }
+        );
+    }
+
+    function get_top_repos( req, res )
+    {
+        database.all(
+            'SELECT * FROM votes ORDER BY votes DESC LIMIT 5',
+            [],
+            function( err, rows ) {
+                if ( err ) {
+                    console.log(err);
+                    res.send("{}");
+                } else {
+                    var result = {
+                        rows: rows
+                    };
+                    console.log("Getting top repos");
+                    res.jsonp(result);
                 }
             }
         );
     }
 
     module.exports.vote_for_repo = vote_for_repo;
+    module.exports.get_top_repos = get_top_repos;
 }());
